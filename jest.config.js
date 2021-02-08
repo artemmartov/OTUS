13b2c85e91826c951570724116f6133a6e2c8439
Artem Geroyan <geroyan.artem@mail.ru>
2021-02-08 11:17:51 +0300

    Обнудение главной ветки

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-  }
-}
